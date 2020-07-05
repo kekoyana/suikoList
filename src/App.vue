9ad5d16ba5228b541c78189198008bccb8842311
kekoyana <kekoyana0@gmail.com>
2020-07-05 11:53:15 +0900

    HeroTableコンポーネントの追加
    参考:
    https://xaksis.github.io/vue-good-table/guide/#usage-with-nuxt-js

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeroTable />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import HeroTable from './components/HeroTable.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    HeroTable,
   }
 }
 </script>
