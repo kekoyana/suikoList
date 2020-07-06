0a84c2d2568067f07bb93c4064962de3a9f577ac
kekoyana <kekoyana0@gmail.com>
2020-07-07 00:09:04 +0900

    能力当てクイズ作った

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -1,9 +1,31 @@
 <template>
   <div class="quiz">
-    <div>ひげ</div>
-    Quiz!!
-    {{ this.heroname }}
-    <button @click="changeName">名前変更</button>
+    <button @click="changeHero">強者変更</button>
+    <div v-if="hero">
+      <dl>
+        <div>
+          <dt>忠義</dt>
+          <dd>{{ this.hero.integrity }}</dd>
+          <dt>仁愛</dt>
+          <dd>{{ this.hero.mercy }}</dd>
+          <dt>勇気</dt>
+          <dd>{{ this.hero.courage }}</dd>
+        </div>
+        <div>
+          <dt>腕力</dt>
+          <dd>{{ this.hero.strength }}</dd>
+          <dt>技量</dt>
+          <dd>{{ this.hero.dexterity }}</dd>
+          <dt>知力</dt>
+          <dd>{{ this.hero.wisdom }}</dd>
+        </div>
+      </dl>
+      <input v-model="inputHero" />
+      <button @click="check">決定</button>
+      {{ message }}
+      <button @click="answer">正解表示</button>
+      <div v-if="answerFlg">{{ this.hero.name }} {{ this.hero.kana }}</div>
+    </div>
   </div>
 </template>
 
@@ -14,15 +36,44 @@ export default {
   name: "Quiz",
   data() {
     return {
-      heroname: "だみー"
+      hero: null,
+      inputHero: "",
+      message: "",
+      answerFlg: false,
+      randCols: ["strength", "dexterity", "wisdom"],
+      columns: {
+        integrity: "忠義",
+        mercy: "仁愛",
+        courage: "勇気",
+        body: "体力",
+        strength: "腕力",
+        dexterity: "技量",
+        wisdom: "知力"
+      }
     };
   },
   methods: {
-    randHero() {
-      return heros.heros[Math.floor(Math.random() * heros.heros.length)];
+    changeHero() {
+      this.answerFlg = false;
+      this.hero = heros.heros[Math.floor(Math.random() * heros.heros.length)];
     },
-    changeName() {
-      this.heroname = this.randHero().name;
+    check() {
+      if (this.inputHero == this.hero.kana) {
+        this.message = "正解";
+      } else {
+        this.message = "まちがい";
+      }
+    },
+    answer() {
+      this.answerFlg = true;
+    },
+    quiz4() {
+      this.randCols = this.columns.keys
+        .slice()
+        .sort(function() {
+          return Math.random() - 0.5;
+        })
+        .slice(0, 3);
     }
   }
 };
